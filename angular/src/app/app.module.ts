75f21ca2fb0e97766e68370df3acf56a65417336
faspen <aspensonf@gmail.com>
2024-07-31 12:40:01 -0500

    first angular push

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CharacterComponent } from './character/character.component';
+import { FactionComponent } from './faction/faction.component';
+import { LocationComponent } from './location/location.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharacterComponent,
+    FactionComponent,
+    LocationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angular/src/app/faction/faction.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faction',
+  templateUrl: './faction.component.html',
+  styleUrl: './faction.component.css'
+})
+export class FactionComponent {
+
+}
