8580888888aba462c47620bff0317bb8817e0aab
faspen <aspensonf@gmail.com>
2024-08-12 12:31:41 -0500

    location view

---
+++ angular/src/app/location/location-modal/location-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-location-modal',
+  templateUrl: './location-modal.component.html',
+  styleUrl: './location-modal.component.css'
+})
+export class LocationModalComponent {
+
+}
---
+++ angular/src/app/location/location.model.ts
@@ -0,0 +1,11 @@
+export class LocationDto {
+    id: number = 0;
+    name: string = '';
+    description: string = '';
+}
+
+export class LocationAddEditDto {
+    id: number = 0;
+    name: string = '';
+    description: string = '';
+}
