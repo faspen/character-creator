aabbcbe5c76b9fff99e1b5d16bba957a08d548d5
faspen <aspensonf@gmail.com>
2024-08-14 10:00:44 -0500

    faction view done

--- angular/src/app/faction/faction.component.ts
+++ angular/src/app/faction/faction.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ConstantsService } from '../constants.service';
+import { FactionAddEditDto, FactionDto } from './faction.model';
 
 @Component({
   selector: 'app-faction',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrl: './faction.component.css'
 })
 export class FactionComponent {
+  @ViewChild('factionModal') factionModal: ElementRef | undefined;
+  factions: FactionDto[] = [];
+  factionToUpdate = new FactionAddEditDto();
+  deletionWarningShow = false;
 
+  constructor(private http: HttpClient, private constants: ConstantsService) {}
+
+  ngOnInit(): void {
+      this.getData();
+  }
+
+  getData() {
+    this.http.get(this.constants.apiUrl + 'faction')
+      .subscribe({
+        next: res => {
+          this.factions = res as FactionDto[];
+        },
+        error: err => console.log(err)
+      });
+  }
+
+  openModal() {
+    // this.modalService.showModal();
+    this.factionToUpdate = new FactionAddEditDto();
+  }
+
+  openForEdit(faction: FactionDto) {
+    this.openModal();
+    this.factionToUpdate = { ...faction };
+  }
+
+  delete(faction: FactionDto) {
+    // if (race.characters.length > 0) {
+    //   this.deletionWarningShow = true;
+    //   setTimeout(() => {
+    //     this.deletionWarningShow = false;
+    //   }, 5000);
+
+    //   return;
+    // }
+    this.http.delete(this.constants.apiUrl + 'faction/' + faction.id)
+      .subscribe({
+        next: res => {
+          this.getData();
+        },
+        error: err => console.log(err)
+      });
+  }
 }
